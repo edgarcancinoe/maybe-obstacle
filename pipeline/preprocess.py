fb550b8fd671ddd8c6d653190ab1a97f5a4b195b
Edgar Cancino <edgarcancinoe@gmail.com>
2025-06-16 11:00:53 +0200

    Update: github restructuration. Conformal prediction notebook with LAF data loading and usage.

--- pipeline/preprocess.py
+++
@@ -1,9 +0,0 @@
-import torchvision.transforms as T
-
-
-def get_default_transforms():
-    return T.Compose([
-        T.ToTensor(),
-        T.Resize((256, 512)),
-        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
